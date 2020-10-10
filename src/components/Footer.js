ae258fb411c296d50c751ca5167b56826c193bff
kim-boyd <kimberlyboyd2@gmail.com>
2020-10-10 19:15:02 -0400

    added footer and socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,12 @@ import "./Footer.css"
 
 export default function Footer() {
     return(
-        <div id="footer">
-
+        <div className="footer">
             <div className="navigation">
-            <a href="https://www.github.com/kimboyd12">
-                    Github</a>
-                <a href="https://www.linkedin.com/in/kim-boyd">LinkedIn</a>
-                <a href="#">kimberlyboyd2@gmail.com</a>
+            <a href="https://www.github.com/kimboyd12"><i class="fab fa-github"></i></a>
+
+                <a href="https://www.linkedin.com/in/kim-boyd"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://www.twitter.com/kimwritescode"><i class="fab fa-twitter"></i></a>
             </div>
 
         </div>
